8a21b3e38d0cd4a93aafba11db29a3018f316c94
shubham vyas <81517539+Shubham-Vy@users.noreply.github.com>
2021-08-27 21:07:34 +0530

    Nix-configuration-file added

---
+++ nixos/configuration.nix
@@ -0,0 +1,21 @@
+{ config, pkgs, ... }:
+
+{
+	imports = 
+  [
+		./hardware-configuration.nix
+		./wm.nix
+		./audio-pipewire.nix
+		./system.nix
+		./package.nix
+	];
+
+ # Define a user account. Don't forget to set a password with ‘passwd’.
+	users.users.xd = {
+		isNormalUser = true;
+		extraGroups = [ "wheel" "audio" "video" "networkmanager"];
+	};
+
+	system.stateVersion = "21.05";
+
+}
