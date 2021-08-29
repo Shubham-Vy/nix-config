3249bf8a99900c62ef07bbdc98a713ca7098e4db
Shubham-Vy <shubhamvyas1691@hotmail.com>
2021-08-29 17:00:27 +0530

    NVIDIA drivers added

---
+++ nixos/python.nix
@@ -0,0 +1,11 @@
+{ config, pkgs, ... }:
+with pkgs;
+let
+  my-python-packages = python-packages: with python-packages; [
+    pandas
+    requests
+    # other python packages you want
+  ]; 
+  python-with-my-packages = python3.withPackages my-python-packages;
+in 
+  {}
